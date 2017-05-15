8ba734f04990ba9992c74b4d2f3bbe5220744b17
Jan-Lucas Ott <ottj3@tcnj.edu>
2017-05-15 13:55:42 -0400

    Output should use newlines not spaces.

--- printArray.c
+++ printArray.c
@@ -11,8 +11,7 @@
 void printArray(int* array, int size) {
     int i;
     for (i=0; i < size; i++)
-        printf("%d ", array[i]);
-    printf("\n");
+        printf("%d\n", array[i]);
 }
 
 #endif
